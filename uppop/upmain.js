58c904d449fba5c876547e90103bb829a60a0275
pa <108421645+3Melody@users.noreply.github.com>
2023-02-08 10:06:58 +0700

    The Last Finalๆ

--- uppop/upmain.js
+++ uppop/upmain.js
@@ -76,9 +76,7 @@ function followed2(){
 
 function addactivity(){
   const createnew = document.createElement('img');
-      createnew.src = "image-uppop/new-add.svg";
+      createnew.src = "image-uppop/new-add-one.svg";
       document.getElementById('new-add').appendChild(createnew);
 }
 
-
-console.log(setting);
